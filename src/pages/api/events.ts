8d23129ad4e07e8f07648914ea7ae0bd33f263d2
Rasukarusan <7gta0you1tube000@gmail.com>
2023-12-19 19:23:43 +0900

    SSE実装

---
+++ src/pages/api/events.ts
@@ -0,0 +1,29 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { text } from './_text';
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  res.setHeader('Content-Type', 'text/event-stream');
+  res.setHeader('Cache-Control', 'no-cache');
+  res.setHeader('Connection', 'keep-alive');
+  res.setHeader('Content-Encoding', 'none');
+
+  const str = text()
+  let index = 0;
+  const intervalId = setInterval(() => {
+    if (index < str.length) {
+      res.write(`data: ${str[index]}\n\n`);
+      index++;
+    } else {
+      clearInterval(intervalId);
+      res.end();
+    }
+  }, 100);
+
+  req.on('close', () => {
+    clearInterval(intervalId);
+    res.end();
+  });
+}
